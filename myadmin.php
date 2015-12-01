c83780210032d962612d40faf6f35b10b5ad115d
eimpec <eimpec@gmail.com>
2015-12-01 16:23:28 +0000

    Some work on all mighty Admin panel (different from User panel)

---
+++ myadmin.php
@@ -0,0 +1,42 @@
+<?php
+	include 'header.php';
+?>
+
+<div class="container-fluid" style="margin-left:20%; margin-right:20%">
+  <div class="row">
+    <div class="col-md-6">
+      <div class="panel panel-default">
+        <div class="panel-body"  style="background-color:#5cb85c">
+          <h1 style="color:white"> Users </h1>
+          <h2 style="color:white"> 9000 </h2>
+        </div>
+      </div>
+    </div>
+    <!-- Modal Look up -->
+    <div class="col-md-6" >
+      <div class="panel panel-default">
+        <div class="panel-body"  style="background-color:#d9534f">
+          <h1 style="color:white"> Quizes </h1>
+          <h2 style="color:white"> 9000 </h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <table class="table table-condensed">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Email </th>
+          <th>Password</th>
+        </tr>
+      </thead>
+      <tr>
+        <td>1</td>
+        <td>smth</td>
+        <td>kalalalala</td>
+      </tr>
+
+    </table>
+  </div>
+</div>
