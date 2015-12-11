7df5196fe09d2fce916efef6aad3f274e82604cf
decyg <declangoldneilson@hotmail.com>
2015-12-11 15:56:24 +0000

    Tidied up my quizzes page, made it use the same style as the all quizzes page with the colours

--- mquiz.php
+++ mquiz.php
@@ -10,27 +10,29 @@
 <div class="container">
 	<div class="jumbotron">
 		<h1>My Quizzes!</h1>
-		<p>Placeholder for list of your own quizzes and ability to edit them.</p>
-		
-		
+		<p>Here's a list of the quizzes you've made!</p>
+
+		<h2>LIST OF YOUR QUESTIONAIRES:</h2>
+		<hr class="colorgraph">
+
 		<?php
-	
-			$query = "SELECT * FROM QUESTS q, USERS u WHERE u.email = \"" . $_SESSION["email"] . "\" AND u.userID = q.createdBy;"; 
+
+			$query = "SELECT * FROM QUESTS q, USERS u WHERE u.email = \"" . $_SESSION["email"] . "\" AND u.userID = q.createdBy;";
 
 			$result = mysqli_query($link, $query) or die(mysqli_error());
-			
-			echo "<h2>LIST OF YOUR QUESTIONAIRES:</h2>";
+
 
 			?>
-			
+
 			<div class="row">
 			<?php while($row = mysqli_fetch_array($result)) : ?>
 				<div class="col-xs-4">
-				<div class="panel panel-default" align="center">
-				<a href="myquizanalysis.php?quizID=<?php echo $row["questID"];?>" style=" text-decoration: none;">
-				<h2> <?php echo $row["questName"]; ?></h2>
-				</a>
-				</div>	
+					<div class="panel panel-default" align="center">
+						<a href="myquizanalysis.php?quizID=<?php echo $row["questID"];?>" style=" text-decoration: none;">
+							<h2> <?php echo $row["questName"]; ?></h2>
+						</a>
+					</div>
+					<hr class="colorgraph">
 				</div>	
 			<?php endwhile; ?>
 			</div>
